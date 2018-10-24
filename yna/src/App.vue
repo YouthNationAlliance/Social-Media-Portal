619cff26afb78ecab8e7fdc9ecbadb8267a4ee81
Kailas Moon <kailasmoon@gmail.com>
2018-10-24 18:37:10 -0400

    Starting on Vue

---
+++ yna/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>YNA</span>
+        <span class="font-weight-light">lliance</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://www.ynalliance.ca/"
+        target="_blank"
+      >
+        <span class="mr-2">Home</span>
+      </v-btn>
+      <v-btn
+        flat
+      >
+        <span class="mr-2">Login</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <Connect/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Connect from './components/Connect'
+
+export default {
+  name: 'App',
+  components: {
+    Connect
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ yna/src/components/Connect.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="text-xs-center">
+    <v-dialog
+      v-model="dialog"
+      width="600"
+      scrollable
+    >
+      <v-btn
+        slot="activator"
+        color="red lighten-2"
+        dark
+      >
+        Click Me
+      </v-btn>
+
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+        <span>Connect with us on social media</span>
+        </v-card-title>
+
+        <v-card-text style="height: 500px;">
+
+        <img src="../assets/modal.png" style="width: 550px"/>
+
+        <v-list two-line>
+        <template v-for="(item, index) in items">
+          <v-subheader
+            v-if="item.header"
+            :key="item.header"
+          >
+            {{ item.header }}
+          </v-subheader>
+
+          <v-divider
+            v-else-if="item.divider"
+            :inset="item.inset"
+            :key="index"
+          ></v-divider>
+
+          <v-list-tile
+            v-else
+            :key="item.title"
+            avatar
+            @click=""
+          >
+            <v-list-tile-avatar>
+              <img :src="item.avatar">
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+              <v-list-tile-title v-html="item.title"></v-list-tile-title>
+              <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        dialog: false,
+        items: [
+         {
+           avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+           title: 'Visit us online',
+           subtitle: "www.ynalliance.ca"
+         },
+         { divider: true, inset: true },
+         {
+           avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+           title: 'Facebook',
+           subtitle: "@teamyna"
+         },
+         { divider: true, inset: true },
+         {
+           avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+           title: 'Instagram',
+           subtitle: "@teamyna"
+         }
+       ]
+      }
+    }
+  }
+</script>
